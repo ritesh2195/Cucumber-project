9d4347479c85306c9df5889f7e0b28138b3a66ff
RITESH RANJAN MISHRA <58725105+ritesh2195@users.noreply.github.com>
2020-01-30 17:09:03 +0530

    Update SelectItem.java

--- src/main/java/CBPack/Page/SelectItem.java
+++ src/main/java/CBPack/Page/SelectItem.java
@@ -1,8 +1,8 @@
  package CBPack.Page;
 
-	import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+     import org.openqa.selenium.WebDriver;
+     import org.openqa.selenium.WebElement;
+     import org.openqa.selenium.support.FindBy;
 	
 	public class SelectItem {
 	
@@ -24,7 +24,7 @@ import org.openqa.selenium.support.FindBy;
 		
 	this.driver=driver;
 	
-	//PageFactory.initElements(driver, this);
+	PageFactory.initElements(driver, this);
 	
 	}
 	
